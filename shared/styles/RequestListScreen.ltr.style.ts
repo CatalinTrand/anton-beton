9596aedb7409addbbdcbdbd3a251a466b0113567
CatalinTrand <catalin.lgg3@gmail.com>
2020-08-24 21:02:14 +0300

    request list v1.0 + bugfixes

--- shared/styles/RequestListScreen.ltr.style.ts
+++ shared/styles/RequestListScreen.ltr.style.ts
@@ -19,6 +19,97 @@ const RequestListScreenLtrStyle = StyleSheet.create({
     fontWeight: 'bold',
     color: Colors.orange,
   },
+  list: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
+  },
+  list_item: {
+    margin: 10,
+    marginTop: 5,
+    marginBottom: 5,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: Colors.grey,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start'
+  },
+  title_contents: {
+    paddingLeft: 10,
+    paddingTop: 5,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  list_item_title: {
+    fontSize: Fonts.h6,
+    color: Colors.orange,
+    fontWeight: 'bold',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  offers_count: {
+    color: Colors.primary,
+    marginLeft: 12,
+    fontSize: Fonts.regular
+  },
+  list_item_details: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+  },
+  list_item_address: {
+    width: '53%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+    padding: 10
+  },
+  list_item_address_title: {
+    fontSize: Fonts.regular,
+    fontWeight: 'bold',
+    paddingBottom: 10,
+  },
+  list_item_address_value: {
+    fontSize: Fonts.small,
+    fontWeight: 'bold',
+    color: Colors.darkGrey,
+  },
+  list_item_additional_details: {
+    width: '47%',
+    padding: 0,
+    paddingRight: 10,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+  },
+  list_item_detail: {
+    paddingTop: 10,
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+  },
+  list_item_detail_title: {
+    fontSize: Fonts.medium,
+    fontWeight: 'bold',
+    color: Colors.black,
+  },
+  list_item_detail_value: {
+    paddingTop: 2,
+    fontSize: Fonts.small,
+    color: Colors.darkGrey,
+  },
 });
 
 export default RequestListScreenLtrStyle;
--- src/screens/requestListScreen.tsx
+++ src/screens/requestListScreen.tsx
@@ -1,5 +1,5 @@
 import RequestListScreenLtrStyle from '../../shared/styles/RequestListScreen.ltr.style';
-import {Text, View} from "react-native";
+import {ScrollView, Text, TouchableOpacity, View} from "react-native";
 import * as React from "react";
 import BottomTabNavigator from "../components/navigation/bottomTabNavigator";
 import {CustomIcons} from "../../shared/themes";
@@ -15,13 +15,57 @@ const RequestListScreen = ({route, navigation}) => {
 
   };
 
+  const requests = [
+    {
+      id: 10005,
+      address: 'Str. Lujerului 42J, Bucuresti, Romania',
+      coordinates: {lat: 45.34, lng: 21.55},
+      quantity: 5000,
+      maxPrice: 30000,
+      offers: 4,
+    },
+    {
+      id: 10006,
+      address: 'Str. Lujerului 42J, Bucuresti, Romania',
+      coordinates: {lat: 45.34, lng: 21.55},
+      quantity: 15000,
+      maxPrice: 300000,
+      offers: 1,
+    },
+    {
+      id: 10007,
+      address: 'Str. Lujerului 42J, Bucuresti, Romania',
+      coordinates: {lat: 45.34, lng: 21.55},
+      quantity: 3400,
+      maxPrice: 35000,
+      offers: 0,
+    },
+    {
+      id: 10008,
+      address: 'Str. Lujerului 42J, Bucuresti, Romania',
+      coordinates: {lat: 45.34, lng: 21.55},
+      quantity: 1000,
+      maxPrice: 10000,
+      offers: 7,
+    },
+    {
+      id: 10009,
+      address: 'Str. Lujerului 42J, Bucuresti, Romania',
+      coordinates: {lat: 45.34, lng: 21.55},
+      quantity: 4000,
+      maxPrice: 20000,
+      offers: 3,
+    },
+  ];
+
   return (
     <View style={RequestListScreenLtrStyle.container}>
       <Header
         placement="left"
+        containerPaddingBottom={10}
         leftComponent={
           <CustomIcons
-            style={{marginTop: 15,marginLeft: 10}}
+            style={{marginTop: 15, marginLeft: 10}}
             size={Fonts.h6}
             color={Colors.black}
             name="cog"
@@ -34,8 +78,35 @@ const RequestListScreen = ({route, navigation}) => {
           </View>
         }
         rightComponent={null}
-        noBorder={true}
+        noBorder={false}
       />
+      <ScrollView contentContainerStyle={RequestListScreenLtrStyle.list}>
+        {requests.map((request, idx) =>
+          <TouchableOpacity key={idx} style={RequestListScreenLtrStyle.list_item} onPress={() => console.log("clicked request id " + request.id)}>
+            <View style={RequestListScreenLtrStyle.title_contents}>
+              <Text style={RequestListScreenLtrStyle.list_item_title}>{I18n.t('request') + " #" + request.id}</Text>
+              <Text style={RequestListScreenLtrStyle.offers_count}>{request.offers + " " + I18n.t('offers')}</Text>
+            </View>
+            <View style={RequestListScreenLtrStyle.list_item_details}>
+              <View style={RequestListScreenLtrStyle.list_item_address}>
+                <Text style={RequestListScreenLtrStyle.list_item_address_title}>{I18n.t('short_address')}</Text>
+                <Text style={RequestListScreenLtrStyle.list_item_address_value}>{request.address}</Text>
+              </View>
+              <View style={RequestListScreenLtrStyle.list_item_additional_details}>
+                <View style={RequestListScreenLtrStyle.list_item_detail}>
+                  <Text style={RequestListScreenLtrStyle.list_item_detail_title}>{I18n.t('quantity')}</Text>
+                  <Text style={RequestListScreenLtrStyle.list_item_detail_value}>{request.quantity + " m³"}</Text>
+                </View>
+                <View style={RequestListScreenLtrStyle.list_item_detail}>
+                  <Text style={RequestListScreenLtrStyle.list_item_detail_title}>{I18n.t('price')}</Text>
+                  <Text style={RequestListScreenLtrStyle.list_item_detail_value}>{request.maxPrice + " RON"}</Text>
+                </View>
+              </View>
+            </View>
+          </TouchableOpacity>
+        )}
+        <View style={{height: 50}}></View>
+      </ScrollView>
       <BottomTabNavigator route={route} navigation={navigation} selected={2}/>
     </View>
   );
